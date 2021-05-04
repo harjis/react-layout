2221f688efdb5e95865e27701c207eb399be49fc
Joonas Harjukallio <harjukallio@gmail.com>
2021-05-04 15:58:08 +0300

    Updates

---
+++ precache-manifest.7b1b8f911b42f7790db8463c4fac8ded.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0490d13f9451e67b09f0fdb64be9608b",
+    "url": "/react-layout/index.html"
+  },
+  {
+    "revision": "c7ccaf192d3534839d17",
+    "url": "/react-layout/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "405d903d5610688de811",
+    "url": "/react-layout/static/css/main.d5fe2203.chunk.css"
+  },
+  {
+    "revision": "c7ccaf192d3534839d17",
+    "url": "/react-layout/static/js/2.5caba5f7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-layout/static/js/2.5caba5f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "405d903d5610688de811",
+    "url": "/react-layout/static/js/main.a47bb558.chunk.js"
+  },
+  {
+    "revision": "0632c26350b5e597503d",
+    "url": "/react-layout/static/js/runtime-main.d8c546c2.js"
+  }
+]);
